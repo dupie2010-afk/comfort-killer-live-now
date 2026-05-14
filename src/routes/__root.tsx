e15ed9cc77f593ec1a33a957319d36a86125277c
dupie2010-afk <dupie2010@gmail.com>
2026-05-14 13:41:15 +0200

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -61,7 +61,7 @@ function RootComponent() {
               hash="challenge"
               className="bg-brand-red text-white px-6 py-2 font-black uppercase tracking-widest text-xs hover:bg-brand-red-light transition-all shadow-lg"
             >
-              Enter the Protocol
+              ACCESS THE SYSTEM
             </Link>
           </div>
 
